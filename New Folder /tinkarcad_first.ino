2c65141ee115222712c3d288730d511eb675744c
Sohanur Rahman Redoy <mdsrredoy@gmail.com>
2025-11-16 03:07:54 +0600

    Update tinkarcad_first.ino

--- New Folder /tinkarcad_first.ino
+++ New Folder /tinkarcad_first.ino
@@ -1,1 +1,20 @@
+void setup()
+{
+ Serial.begin(9600);
+}
 
+void loop()
+{
+  Serial.print("Hello world");
+}
+//output pasapasi
+void setup()
+{
+ Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("Hello world");
+}
+//output line by line
