f81d43afb9c552414b0de7ec1dbdd0321220d217
Sohanur Rahman Redoy <mdsrredoy@gmail.com>
2025-11-16 03:53:21 +0600

    Update and rename tinkarcad code learning to Variable_learning.ino

---
+++ New Folder /Variable_learning.ino
@@ -0,0 +1,44 @@
+//type of variable
+//intteger 1,2,10
+//float 9.8,3.1416
+//character 'a','b','c'
+//string "red","Ball","Car","Hello World"
+//variable declaration korte hoile age variable type  dite hbe tarpor variable name dewa laghbe
+//variable name er sorote number dewa jbe na,underscore dewa jabe kintu %,& ei sign guls dewa jbe na,maximum khetre variable er first latter small letter e dewa hoi
+
+void setup()
+{
+ Serial.begin(9600);
+ int redoy= 2;
+ float rahi;
+ char miyadad;
+ String ishrak;
+ Serial.print(redoy);
+
+}
+
+void loop()
+{
+  
+}
+//output =2
+
+
+void setup()
+{
+ Serial.begin(9600);
+ int redoy;
+ float rahi;
+ char miyadad;
+ String ishrak;
+ redoy=2;
+ Serial.print(redoy);
+
+}
+
+void loop()
+{
+  
+}
+//OUTPUT same
+
