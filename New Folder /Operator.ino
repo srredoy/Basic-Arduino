5708202de081062e53f29bd0206c6760def58a06
Sohanur Rahman Redoy <mdsrredoy@gmail.com>
2025-11-16 05:01:27 +0600

    Update and rename Operator to Operator.ino

---
+++ New Folder /Operator.ino
@@ -0,0 +1,16 @@
+//Operator type: Arithmatc operator,comparison operator,Boolean Operator
+//Arithmatci: +.-.*,/
+//comparison: <,>,<=,>=,==,!=
+//Boolean:&&(AND0, ||(OR)
+void setup()
+{
+ Serial.begin(9600);
+ int a=3,b=4;
+ int sum=a+b;
+ Serial.print(sum);
+}
+
+void loop()
+{
+}
+//OUTPUT=7
