d35b2791b2067a2a73292a31a0f3e5f9a3135138
Sohanur Rahman Redoy <mdsrredoy@gmail.com>
2025-11-16 18:46:22 +0600

    Update Taking Input.ino

--- New Folder /Taking Input.ino
+++ New Folder /Taking Input.ino
@@ -11,6 +11,11 @@ Serial.println(a);
 }
 //OUTPUT=00010,jei input newa hbe seitai ashbe
 
+
+
+
+
+
 //character taking
 void setup()
 {
@@ -24,6 +29,23 @@ char a=Serial.read();
 Serial.println(a);
 delay(1000);
 } 
+
+
+
+//String taking
+void setup()
+{
+Serial.begin(9600);
+ 
+}
+
+void loop()
+{
+String a=Serial.readString();
+Serial.println(a);
+delay(1000);
+} 
+
 
 
 
